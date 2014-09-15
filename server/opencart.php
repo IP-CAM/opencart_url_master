b710123c86b7823e62e827136c6b4b0db6ce12af
developersuraj <mail.surajjain@gmail.com>
2014-09-15 15:33:37 +0530

    Create opencart.php

---
+++ server/opencart.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+$opencart = array(
+	'DIR' => array(		
+				'admin/controller/',
+				'admin/view/template/',
+				'catalog/controller/'
+				),
+	'FILE' => array(
+				'htaccess.txt',
+				'.htaccess',
+				'index.php',
+				'admin/index.php',
+				'system/library/url.php',			
+				'admin/view/javascript/common.js',
+				'catalog/view/javascript/common.js'						
+				)
+);
+
+$ocConfig = array(
+	'FILE' => array(
+				'config.php',
+				'admin/config.php'						
+				)
+);
+
+/*
+/ Functions
+*/
+function _updateOCFile($text, $replace_text, $file, $urlPattern) {
+	$old = array("'" . $text . "'", $text . "=");
+	$new = array("'" . $replace_text . "'", $replace_text . "=");
+	if(file_exists($file)) {	
+		$fileStr = file_get_contents($file);
+		$fileStr = str_replace($old, $new, $fileStr, $count);
+		
+		if($urlPattern === 'index') {
+			$indexO = array('?' . $replace_text . '=');
+			$indexN = array('index.php?' . $replace_text . '=');
+			$fileStr = str_replace($indexO, $indexN, $fileStr);
+		}
+		else if($urlPattern === 'noindex') {
+			$indexO = array('index.php?' . $replace_text . '=');
+			$indexN = array('?' . $replace_text . '=');
+			$fileStr = str_replace($indexO, $indexN, $fileStr);
+		}
+		
+		file_put_contents($file, $fileStr);
+		return $count;
+	}
+	return false;
+}
+
+function _getOCLocalValue($file) {
+	if(file_exists($file)) {	
+		$fileStr = file_get_contents($file);
+		$break = explode('new Action($request->get[\'', $fileStr);
+		if(isset($break[1])) { return explode('\']', $break[1])[0]; }
+		return false;
+	}
+	return false;
+}
+
+function _getOCLocalUrl($file, $val) {
+	if(file_exists($file)) {	
+		$fileStr = file_get_contents($file);
+		return (strpos($fileStr, 'index.php?' . $val . '=') !== false ? 'index' : 'noindex');
+	}
+	return false;
+}
+
+function scan($str) {
+	return substr(preg_replace("/[^a-zA-Z0-9_]+/", "",trim($str)), 0, 10);
+}
+
+function _filter($str) {
+	$value = strtoupper(str_replace(array('_','-'), ' ', trim(explode('.php', $str)[0])));
+	return ucwords(strtolower(($value === 'INDEX' ? 'CHANGE URL PATTERN' : $value)));
+}
+
+?>
